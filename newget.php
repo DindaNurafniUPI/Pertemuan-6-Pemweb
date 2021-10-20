321c12d255c8cf495a5bf5108db059ad801eebb7
Dinda Nurafni <90329718+DindaNurafniUPI@users.noreply.github.com>
2021-10-20 20:55:28 +0700

    Add files via upload

---
+++ newget.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body bgcolor="gray">
+<h1>Silahkan Isi Form Anda dengan benar</h1>
+    <hr>
+  <form name="" action="get.php" method="get">
+    <table>
+      <tr>
+        <td>  Nomor : FTL-PPNBr-TKPU- </td>
+        <td> <input type = "text" name = "nomor"> </td>
+    </tr>
+    <tr>
+        <td> 1. Nama Pemilik/Nahkoda = </td>
+        <td> <input type = "text" name = "nama"> </td>
+    </tr>
+    <tr>
+        <td> 2. Alamat = </td>
+        <td> <input type = "text" name = "alamat"> </td>
+    </tr>
+    <tr>
+        <td> 3. Nama Kapal = </td>
+        <td> <input type = "text" name = "nakap"> </td>
+    </tr>
+    <tr>
+        <td> 4. Tanda Selar = </td>
+        <td> <input type = "text" name = "tasel"> </td>
+    </tr>
+    <tr>
+      <td> 5. Ukuran Kapal  </td>
+      <tr> 
+          <td>a. Panjang Kapal= </b></td>
+          <td> <input type = "text" name = "papal"> </td>
+          <td> M </td>
+      </tr>  
+      <tr> 
+        <td>b. Berat Kotor = </b></td>
+        <td> <input type = "text" name = "betor"> </td>
+        <td> GT/DWT </td>
+    </tr>  
+    <tr> 
+      <td>c. Kekuatan Mesin = </b></td>
+      <td> <input type = "text" name = "kesin"> </td>
+      <td> PK/HP </td>
+  </tr>  <br>
+  <br>
+  <tr>
+    <td><i>Pemilik/Nahkoda</i></td>
+    <td> <input type = "text" name = "pem"> </td>
+    </tr><br>
+  <br>
+    </table>
+    <br>
+    <td colspan="3"><input class="px-3" type="submit" value="simpan" name="tombol" ></td>
+
+  </form>
+  <br>
+  <main class="px-3">
+    <h1>Isi form layanan untuk kapal anda </h1>
+    <p class="lead">Kapal anda tentu menjadi aset berharga anda, jangan lupa isi form untuk layanan kapal anda, Terimakasih</p>
+    <p class="lead">
+      <a href="https://www.kkp.go.id/" class="btn btn-lg btn-secondary fw-bold border-white bg-gray">Learn more about KKP</a>
+    </p>
+  </main>
+</div>
+</body>
+</html>
---
+++ tugaspost.php
@@ -0,0 +1,177 @@
+<!DOCTYPE html>
+<html lang="en" class="h-100"><head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+    <meta name="generator" content="Hugo 0.88.1">
+    <title>Cover Template · Bootstrap v5.1</title>
+
+    <link rel="canonical" href="https://getbootstrap.com/docs/5.1/examples/cover/">
+
+    
+
+    <!-- Bootstrap core CSS -->
+<link href="/docs/5.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    <!-- Favicons -->
+<link rel="apple-touch-icon" href="/docs/5.1/assets/img/favicons/apple-touch-icon.png" sizes="180x180">
+<link rel="icon" href="/docs/5.1/assets/img/favicons/favicon-32x32.png" sizes="32x32" type="image/png">
+<link rel="icon" href="/docs/5.1/assets/img/favicons/favicon-16x16.png" sizes="16x16" type="image/png">
+<link rel="manifest" href="/docs/5.1/assets/img/favicons/manifest.json">
+<link rel="mask-icon" href="/docs/5.1/assets/img/favicons/safari-pinned-tab.svg" color="#7952b3">
+<link rel="icon" href="/docs/5.1/assets/img/favicons/favicon.ico">
+<meta name="theme-color" content="#7952b3">
+
+
+    <style>
+      .bd-placeholder-img {
+        font-size: 1.125rem;
+        text-anchor: middle;
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        user-select: none;
+      }
+
+      @media (min-width: 768px) {
+        .bd-placeholder-img-lg {
+          font-size: 3.5rem;
+        }
+      }
+    </style>
+
+    
+    <!-- Custom styles for this template -->
+    <link href="cover.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+  </head>
+  <body class="d-flex h-100 text-center text-white bg-dark" data-new-gr-c-s-check-loaded="14.1034.0" data-gr-ext-installed="">
+    
+<div class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
+  <header class="mb-auto">
+    <div>
+      <h3 class="float-md-start mb-0">Form Permintaan layanan kapal</h3>
+      <br>
+      <img src="https://e7.pngegg.com/pngimages/1011/975/png-clipart-ship-s-wheel-graphics-boat-logo-ship.png" height="150" width="198"></img>
+      
+      <nav class="nav nav-masthead justify-content-center float-md-end">
+        <a class="nav-link" href="https://kkp.go.id/djprl/lkkpnpekanbaru/page/453-unduh-formulir-layanan">Features</a>
+        <a class="nav-link" href="https://www.instagram.com/dindafni.i">Contact</a>
+      </nav>
+    </div>
+  </header>
+
+  <form name="" action="post.php" method="post">
+
+  <a class="nav-link active" aria-current="page" href="newpost.php" size="5">Form</a>
+
+  </form>
+  <br>
+  <main class="px-3">
+    <h1>Isi form layanan untuk kapal anda </h1>
+    <p class="lead">Kapal anda tentu menjadi aset berharga anda, jangan lupa isi form untuk layanan kapal anda, klik tulisan form untuk masuk ke form anda, Terimakasih</p>
+    <p class="lead">
+      <a href="https://www.kkp.go.id/" class="btn btn-lg btn-secondary fw-bold border-white bg-gray">Learn more about KKP</a>
+    </p>
+    <br>
+    
+     
+    <div class="row flex-lg-row-reverse align-items-center g-5 py-5">
+        <div class="col-10 col-sm-8 col-lg-6">
+          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3967.0370422558217!2d106.1444745141367!3d-6.125717661768545!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e418adaa4f7f563%3A0x950ec58123df8596!2sUniversitas%20Pendidikan%20Indonesia%20(UPI)%20Kampus%20Serang!5e0!3m2!1sen!2sid!4v1633797829614!5m2!1sen!2sid" 
+          width="250" height="550" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+        </div>
+        <div class="col-lg-6">
+          <div class="col-md-10 mx-auto col-lg-8">
+            <h1> Tanyakan sesuatu</h1>
+            <div class="form-floating mb-3">
+              <input type="name" class="form-control" id="floatingInput" placeholder="xxxxxx xxxxxx">
+              <label for="floatingInput">Nama</label></div>
+              <div class="form-floating mb-3">
+                <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+                <label for="floatingInput">Email address</label>
+              </div>
+              <div class="form-floating mb-7">
+                <div class="mb-3">
+                  <label for="exampleFormControlTextarea1" class="form-label">Pesan</label>
+                  <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                </div>
+            <button class="w-100 btn btn-lg btn-primary" >Kirim</button>
+          </div>
+          </div>
+        </div>
+      </div>
+        </div>
+  </main>
+</div>
+<p id="demo"></p>
+ <br>  
+
+ <div class="container px-4 py-5" id="custom-cards">
+    <h2 class="pb-2 border-bottom">Kapal Nelayan</h2>
+    <div class="col">
+        <div class="card card-cover h-100 overflow-hidden text-white bg-dark rounded-5 shadow-lg" style="background-image: url('https://assets.promediateknologi.com/crop/0x0:0x0/750x500/photo/ayobatang/images/post/articles/2020/05/05/753/kapal_nelayan.jpeg');">
+          <div class="d-flex flex-column h-100 p-5 pb-3 text-shadow-1">
+            <h2 class="pt-5 mt-5 mb-4 display-6 lh-1 fw-bold">Another longer title belongs here</h2>
+            <ul class="d-flex list-unstyled mt-auto">
+              <li class="me-auto">
+                <img src="https://github.com/twbs.png" alt="Bootstrap" width="32" height="32" class="rounded-circle border border-white">
+              </li>
+              <li class="d-flex align-items-center me-3">
+                <svg class="bi me-2" width="1em" height="1em"><use xlink:href="#geo-fill"></use></svg>
+                <small>Bali</small>
+              </li>
+              <li class="d-flex align-items-center">
+                <svg class="bi me-2" width="1em" height="1em"><use xlink:href="#calendar3"></use></svg>
+                <small>5d</small>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+<br>
+      <div class="col">
+        <div class="card card-cover h-100 overflow-hidden text-white bg-dark rounded-5 shadow-lg" style="background-image: url('https://cdn-2.tstatic.net/tribunnews/foto/bank/images/kapal-nelayan-pekalongan1.jpg');">
+          <div class="d-flex flex-column h-100 p-5 pb-3 text-shadow-1">
+            <h2 class="pt-5 mt-5 mb-4 display-6 lh-1 fw-bold">Kapal Nelayan</h2>
+            <ul class="d-flex list-unstyled mt-auto">
+              <li class="me-auto">
+                <img src="https://metropolis.id/files/images/20200310-img20200110084109-1-compress82.jpg" alt="Bootstrap" width="32" height="32" class="rounded-circle border border-white">
+              </li>
+              <li class="d-flex align-items-center me-3">
+                <svg class="bi me-2" width="1em" height="1em"><use xlink:href="#geo-fill"></use></svg>
+                <small>Pekalongan</small>
+              </li>
+              <li class="d-flex align-items-center">
+                <svg class="bi me-2" width="1em" height="1em"><use xlink:href="#calendar3"></use></svg>
+                <small>5d</small>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+<br>
+      <div class="col">
+        <div class="card card-cover h-100 overflow-hidden text-white bg-dark rounded-5 shadow-lg" style="background-image: url('https://seputarkepri.co.id/wp-content/uploads/2020/09/IMG-20200914-WA0286.jpg');">
+          <div class="d-flex flex-column h-100 p-5 pb-3 text-shadow-1">
+            <h2 class="pt-5 mt-5 mb-4 display-6 lh-1 fw-bold">Kapal nelayan </h2>
+            <ul class="d-flex list-unstyled mt-auto">
+              <li class="me-auto">
+                <img src="https://github.com/twbs.png" alt="Bootstrap" width="32" height="32" class="rounded-circle border border-white">
+              </li>
+              <li class="d-flex align-items-center me-3">
+                <svg class="bi me-2" width="1em" height="1em"><use xlink:href="#geo-fill"></use></svg>
+                <small>Nelitung</small>
+              </li>
+              <li class="d-flex align-items-center">
+                <svg class="bi me-2" width="1em" height="1em"><use xlink:href="#calendar3"></use></svg>
+                <small>5d</small>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  </body><grammarly-desktop-integration data-grammarly-shadow-root="true"></grammarly-desktop-integration></html>
